e7b29829601f7fe4f6d039450bac16579365df7a
Axel <dsg4857@gmail.com>
2026-04-12 23:13:46 -0500

    add: Implementar sección de héroe y proyectos, integrar fuentes personalizadas y configurar Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Inter",
+      cssVariable: "--font-Inter",
+      options: {
+        variants: [
+          {
+            weight: "400 500 600 700 800 900",
+            style: "normal",
+            src: ["./src/assets/fonts/inter-variable.woff2"],
+          }
+        ]
+      }
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Akira Expanded",
+      cssVariable: "--font-AkiraExpanded",
+      options: {
+        variants: [
+          {
+            weight: "400 500 600 700 800 900",
+            style: "normal",
+            src: ["./src/assets/fonts/akira-expanded.woff2"],
+          }
+        ]
+      }
+    }
+  ]
+}
+);
